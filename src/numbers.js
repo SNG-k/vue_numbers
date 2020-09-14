9b89511544b39084382bbc44f7c76e89ce15ed64
kousuke.sunaga <kosuke.sunaga@onsuku.jp>
2020-09-14 18:42:46 +0900

    スマホデザイン

--- src/numbers.js
+++ src/numbers.js
@@ -4,7 +4,7 @@ export function numbers() {
         data:{
             ruleShow:false,
             start:false,
-            number:['0','1','2','3','4','5','6','7','8','9'],
+            number:['1','2','3','4','5','6','7','8','9','0'],
             correct:'',
             inputNumber:'',
             answer1:'',
